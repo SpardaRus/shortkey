c9c528b465b1d6696b4dd1ed3e8f41f5e7a031d4
Ясаков Алексей <aleksey.yasakov@gazprombank.ru>
2024-06-23 18:56:38 +0400

    Первый коммит

---
+++ src/main/java/ru/spardarus/shortkey/ShortkeyApplication.java
@@ -0,0 +1,13 @@
+package ru.spardarus.shortkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortkeyApplication.class, args);
+    }
+
+}
